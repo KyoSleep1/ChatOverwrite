cb2a783d79d3d2e27647cc4b1c99e572383fb2b9
KyoSleep <83037839+KyoSleep1@users.noreply.github.com>
2022-11-17 20:40:20 -0700

    Removed Player's Display Name from fetched message

--- BetterChat/src/main/java/dev/sleep/betterchat/client/gui/chat/ChatScreenContainer.java
+++ BetterChat/src/main/java/dev/sleep/betterchat/client/gui/chat/ChatScreenContainer.java
@@ -6,8 +6,11 @@ import dev.sleep.betterchat.mixin.client.MinecraftAccessor;
 import lombok.Getter;
 import net.minecraft.client.GuiMessage;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 //This class works as a storage for the buttons
 public class ChatScreenContainer {
@@ -15,25 +18,56 @@ public class ChatScreenContainer {
     @Getter
     private static final ChatButton EditButton = new ChatButton("TEST 1", 1, 8, 7, 6, 2, 18,
             18, 18, 12, 12, (messageList, chatButton) -> {
-        openChat(ChatScreenContainer.getMessageText(messageList));
+        ChatScreenContainer.openChat(ChatScreenContainer.getMessageTextAndFormat(messageList));
     });
 
     @Getter
     private static final ChatButton DeleteButton = new ChatButton("TEST 2", 5, 8, 3, 6, 2, 2,
             19, 2, 11, 12, (messageList, chatButton) -> {
-        openChat(ChatScreenContainer.getMessageText(messageList));
+        ChatScreenContainer.openChat(ChatScreenContainer.getMessageTextAndFormat(messageList));
     });
 
     private static void openChat(String defaultText) {
         ((MinecraftAccessor) Minecraft.getInstance()).openChat(defaultText);
     }
 
-    private static String getMessageText(List<GuiMessage> messageList) {
+    // Used to get the player message without the actual player's DisplayName (Extra steps are used to ensure chat plugins/mods compatibility)
+    private static String getMessageTextAndFormat(List<GuiMessage> messageList) {
         GuiMessage selectedMessage = ClientChatHandler.getLastFetchedMessage(messageList);
         if (selectedMessage == null) {
             return "";
         }
 
-        return ClientChatHandler.getLegibleText(selectedMessage.content().getVisualOrderText());
+        return ChatScreenContainer.getFormattedText(ClientChatHandler.getLegibleText(selectedMessage.content().
+                getVisualOrderText()).split(" "), 0);
+    }
+
+    /* Used to get the player message without the actual player's DisplayName
+     * Extra steps are used to ensure compatibility with plugins/mods that change the chat structure
+     * Example:
+     *      Original: <Player468> Hi!
+     *      Modified: > Player468 - Hi!
+     */
+    private static String getFormattedText(String[] splittedText, int startIndex) {
+        Component playerDisplayName = Objects.requireNonNull(Minecraft.getInstance().player).getDisplayName();
+        String messageLine = splittedText[startIndex];
+
+        if (!messageLine.contains(playerDisplayName.getString())) {
+            startIndex++;
+            return ChatScreenContainer.getFormattedText(splittedText, startIndex);
+        }
+
+        startIndex++;
+        return buildMessage(splittedText, startIndex);
+    }
+
+    private static String buildMessage(String[] splittedText, int startIndex) {
+        StringJoiner joiner = new StringJoiner(" ");
+
+        for (int i = startIndex; i < splittedText.length; i++) {
+            joiner.add(splittedText[i]);
+        }
+
+        return joiner.toString();
     }
 }
